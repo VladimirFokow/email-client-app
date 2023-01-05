b225a42af81b26a2cd8d863bea27b894eb739644
Vladimir Fokow <fokow.vladimir@gmail.com>
2023-01-05 22:33:18 +0100

    working version - 2 sidebars

--- email-client-app/app.py
+++ email-client-app/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect
 from urllib.parse import unquote_plus  # url variable to normal text
 
+# TODO: create login
+# TODO: replace "Your email" with your actual email
+
 app = Flask(__name__)
 
 DEFAULT_FOLDERS = ['inbox', 'sent', 'drafts', 'bin']
@@ -8,16 +11,16 @@ USER_FOLDERS = ['Folder 1', 'Folder 2']  # TODO: make empty  # TODO: store them 
 
 @app.route('/')
 def index():
-    return redirect('/inbox')
+    return redirect('/inbox/')
 
 
-@app.route('/<folder>')
+@app.route('/<folder>/')
 def access_folder(folder):
     folder_name = unquote_plus(folder)
     print(folder_name)
     if folder_name not in DEFAULT_FOLDERS + USER_FOLDERS:
-        return redirect('/inbox')
+        return redirect('/inbox/')
     # emails = db ...(folder_name)
-    return render_template('dashboard.html', user_folders=USER_FOLDERS)  # and activate the respective folder
+    return render_template('dashboard.html', user_folders=USER_FOLDERS)  # and activate the respective folder (change the active property)
 
 
