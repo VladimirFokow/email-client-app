9ea098b05055de755893121fc89f4f6327b49e24
Vladimir Fokow <fokow.vladimir@gmail.com>
2023-01-05 19:47:23 +0100

    work in progress

--- README.md
+++ README.md
@@ -6,10 +6,12 @@ This is a web application that uses Flask for backend.
 
 Task:
 
-<img src="img/task.png" alt="task" width="200"/>
+<img src="img/task.png" alt="task" width="600"/>
 
 
 ---
 
-Created as a course project at KPI University (Bachelor's 3rd year, 1st semester, for class ТРПЗ - "Технології розроблення програмного забезпечення").
+Created as a course project for KPI University:
+
+Bachelor's 3rd year, 1st semester, class ТРПЗ - "Технології розроблення програмного забезпечення".
 
---
+++ email-client-app/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect
+from urllib.parse import unquote_plus  # url variable to normal text
+
+app = Flask(__name__)
+
+DEFAULT_FOLDERS = ['inbox', 'sent', 'drafts', 'bin']
+USER_FOLDERS = ['Folder 1', 'Folder 2']  # TODO: make empty  # TODO: store them in the db, and access from db at the start
+
+@app.route('/')
+def index():
+    return redirect('/inbox')
+
+
+@app.route('/<folder>')
+def access_folder(folder):
+    folder_name = unquote_plus(folder)
+    print(folder_name)
+    if folder_name not in DEFAULT_FOLDERS + USER_FOLDERS:
+        return redirect('/inbox')
+    # emails = db ...(folder_name)
+    return render_template('dashboard.html', user_folders=USER_FOLDERS)  # and activate the respective folder
+
+
