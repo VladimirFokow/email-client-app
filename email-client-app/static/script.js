eb97751d433950b139d9c6350b38945b65d8a27f
Vladimir Fokow <fokow.vladimir@gmail.com>
2023-01-06 08:18:21 +0100

    email list + copy on click

--- email-client-app/static/script.js
+++ email-client-app/static/script.js
@@ -6,8 +6,45 @@
 }())  // it is IIFE (Immediately Invoked Function Expression) - it is called immediately after definition because of the () at the end
 
 
+// Function that accepts a string and copies it to the clipboard:
+function copyToClipboard(text) {
+  navigator.clipboard.writeText(text)
+}
+
+// Activate the tooltips (hints when hovering over elements):
+var tooltipTriggerList = [].slice.call($('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+var emailTooltip = tooltipList[0]
 
 
+// Function that accepts a string and writes it to the emailTooltip:
+function writeToTooltip(text) {
+  // Note: a hacky solution:
+  $("#navbar-email").attr("title", text)
+  $("#navbar-email").attr("data-bs-original-title", text)
+  emailTooltip._getTipElement().innerText = text
 
+  // If mouse is hovering over $("#navbar-email"):
+  if ($("#navbar-email").is(":hover")) {
+    emailTooltip.show()
+  }
+}
+
+
+// Add the onclick event listener to the $("#navbar-email"):
+$("#navbar-email").click(function() {
+  copyToClipboard($("#navbar-email").text())
+  
+  // Write "Copied!" it to the emailTooltip:
+  writeToTooltip("Copied!")
+  
+  // Wait for 2 seconds, and write "Click to copy" to the emailTooltip:
+  setTimeout(function() {
+    writeToTooltip("Click to copy")
+  }
+  , 2000)
+})
 
 
